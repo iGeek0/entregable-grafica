2f102cee7109c2b1649f4666a95674de3b86f259
iGeek <jesus.cardenas.d@gmail.com>
2023-05-02 20:51:08 -0600

    agregada api

--- src/acquisitions.js
+++ src/acquisitions.js
@@ -1,20 +1,25 @@
 import Chart from 'chart.js/auto';
 import {incrementarValor} from './funciones.js';
+import { getAquisitionsByYear } from './api';
 
 (async function () {
-    const data = [
-        { year: 2010, count: 10 },
-        { year: 2011, count: 20 },
-        { year: 2012, count: 15 },
-        { year: 2013, count: 25 },
-        { year: 2014, count: 22 },
-        { year: 2015, count: 30 },
-        { year: 2016, count: 28 },
-    ].map(fila => ( { ...fila, count: incrementarValor(fila.count) } ) );
+
+    // const data = [
+    //     { year: 2010, count: 10 },
+    //     { year: 2011, count: 20 },
+    //     { year: 2012, count: 15 },
+    //     { year: 2013, count: 25 },
+    //     { year: 2014, count: 22 },
+    //     { year: 2015, count: 30 },
+    //     { year: 2016, count: 28 },
+    // ].map(fila => ( { ...fila, count: incrementarValor(fila.count) } ) );
+
+    const data = await getAquisitionsByYear();
 
     new Chart(
         document.getElementById('acquisitions'),
         {
+            // bar, line, pie, doughnut, radar, polarArea, bubble
             type: 'bar',
             data: {
                 labels: data.map(row => row.year),
